6b3d15e3f666ebf0bfb45eac4002b0bd93701658
Night Fury <sinhvienbk13@gmail.com>
2017-04-24 22:08:17 +0700

    Add project files.

---
+++ TestTravis.Test/TestCase.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using
+
+namespace TestTravis.Test
+{
+    public class TestCase
+    {
+        [TestFixture]
+        class MyTestCase
+        {
+            [TestCase]
+            public void Add()
+            {
+                MyMath math = new MyMath();
+                Assert.AreEqual(31, math.Add(20, 11));
+            }
+
+            [TestCase]
+            public void Sub()
+            {
+                MyMath math = new MyMath();
+                Assert.AreEqual(10, math.Sub(20, 10));
+            }
+        }
+    }
+}
---
+++ TestTravis/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTravis
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b * 2;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
